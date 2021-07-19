48cd600a5bed1fc4207a9a7766aab9158000308c
multibug <multibug90@gmail.com>
2021-07-19 19:21:41 +0300

    test

--- cars/models.py
+++ cars/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Car(models.Model):
-    vin = models.CharField(verbose_name='VIN', db_index=True, max_length=64)
+    vin = models.CharField(verbose_name='VIN', db_index=True, unique=True, max_length=64)
     color = models.CharField(verbose_name='Цвет', max_length=64)
     brand = models.CharField(verbose_name='Брэнд', max_length=64)
     car_types = (
@@ -12,3 +14,4 @@ class Car(models.Model):
         (4, 'Купе'),
     )
     car_type = models.IntegerField(verbose_name='Кузов', choices=car_types)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
